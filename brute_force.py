1f3e32a38db0f06f494584fbf612564e4993336d
Lorenzo Vitali <57009875+Darkenar94@users.noreply.github.com>
2021-05-09 15:45:29 +0200

    aggiunte nuove funzionalità

--- brute_force.py
+++ brute_force.py
@@ -7,39 +7,40 @@ dizionario = file.read()
 file.close()
 
 lista_stringhe = pulisci(dizionario)
-lista_combinazioni = [["numero", "parola", "numero"], 
-["numero", "numero", "parola"], ["numero", "parola", "parola"], 
-["parola", "numero", "parola"], ["parola", "parola", "numero"], 
+lista_combinazioni = [["numero", "parola", "numero"],
+["numero", "numero", "parola"], ["numero", "parola", "parola"],
+["parola", "numero", "parola"], ["parola", "parola", "numero"],
 ["parola", "numero", "numero"]]
 
-pwd = ""
 password = ""
-caratteri = 5
-
+minimo_caratteri = 5
+massimo_caratteri = 20
+operazione_in_corso = "cracking...\n"
+operazione_effettuata = "password craccata.\n"
 testate = []
 
 in_esecuzione = True
 while in_esecuzione:
-    caratteri += 1
-    if not caratteri > 20:
-        for i in range(100): # 100 parole per test
-            pwd = ""
-            pwd = test_casaccio(caratteri, lista_stringhe, pwd, testate)
-            print(pwd)
+    minimo_caratteri += 1
+    if not minimo_caratteri > massimo_caratteri:
+        for i in range(100):
+            pwd = test_casaccio(minimo_caratteri, lista_stringhe, "", testate)
+            mostra_interfaccia(pwd, massimo_caratteri, operazione_in_corso)
+            #print(pwd)
             if accesso(pwd):
                 password = pwd
                 in_esecuzione = False
-            parole = test_ragionevole(lista_combinazioni, caratteri, lista_stringhe, "")
-            for parola in parole:
-                if not parola in testate:
-                    testate.append(parola)
-                    pwd = parola
-                    print(pwd)
+            lista_pwds = test_ragionevole(lista_combinazioni, minimo_caratteri, lista_stringhe, "")
+            for pwd in lista_pwds:
+                if not pwd in testate:
+                    testate.append(pwd)
+                    mostra_interfaccia(pwd, massimo_caratteri, operazione_in_corso)
+                    #print(pwd)
                     if accesso(pwd):
                         password = pwd
                         in_esecuzione = False
     else:
-        caratteri = 5
-        
-print("\npassword: " + password)
+        minimo_caratteri = 5
+
+mostra_interfaccia(password, massimo_caratteri, operazione_effettuata)
 input()
